9ac9c127da7737465edd9092551c63116b6ed851
barnaby69 <95702634+barnaby69@users.noreply.github.com>
2022-03-02 15:42:29 +0000

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,48 +1,64 @@
-# importing datetime, holidays and assign key datetime variables
+# importing datetime and holidays
+# may need to install holidays
+# https://packaging.python.org/en/latest/tutorials/installing-packages/#use-pip-for-installing
 
 from datetime import datetime
-
-# in terminal: python3 -m pip install https://packaging.python.org/en/
-# latest/tutorials/installing-packages/#use-pip-for-installing
-
 import holidays
-
 import string
-
 import secrets  # can also use random however not cryptographically secure
+
+# assigning key datetime variables
 
 now = datetime.now()
 hour = now.hour
 year = now.year
 d = datetime.date(now)
 day = now.strftime("%A")
+
 # checking the time of day
+
 if hour < 12:
     greeting = "Good morning"
 elif hour < 18:
     greeting = "Good afternoon"
 else:
     greeting = "Good evening"
+
 # system greets the user
+
 print(greeting, "\n")
 print("Welcome to python password generator", "\n")
-# password type from user
+
+# password types:
+
 normal = string.ascii_letters + string.digits + string.punctuation
 num_only = string.digits
 no_special_characters = string.ascii_letters + string.digits
+
+# creating list (called password_type) with each password type
+
 password_type = [normal, num_only, no_special_characters]
-password_id = int(input("Please enter a password type (normal = 0; number only \
-= 1; and no special\n characters = 2): "))
+
+# selecting from the list (password_type) with input from the user
+
+password_id = int(input("Please enter a password type (normal = 0; number only = 1; and no special\n characters = 2): "))
 print(" ")
-# password length from user
+
+# selecting a password length with input from the user
+
 length = int(input("Enter password length: "))
 print(" ")
-# picking random characters from the list
-password = "".join(secrets.choice(password_type[password_id]) \
-                   for i in range(length))  # i is a temp variable
+
+# picking random characters, were the type of character is dictated by the selected password type:
+
+password = "".join(secrets.choice(password_type[password_id]) for i in range(length))  # i is a temp variable
+
+# desplaying the password:
+
 print("Your password is:", password, "\n")
-# the system wishes the user a good holiday, weekend or day dependent on
-# today's date
+
+# the system wishes the user a good holiday, weekend or day dependent on today's date:
+
 check_for_holiday = d in holidays.England()
 check_for_weekend = day == "Sunday" or "Saturday"
 if check_for_holiday == True:
